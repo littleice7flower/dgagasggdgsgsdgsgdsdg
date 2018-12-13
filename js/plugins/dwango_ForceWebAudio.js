2e492c85830f6920ce1f5d3cd3105c13f6105e70
littleice7flower <littleice7flower@gmail.com>
2018-12-13 12:10:12 +0900

    20181213

---
+++ js/plugins/dwango_ForceWebAudio.js
@@ -0,0 +1,16 @@
+//=============================================================================
+// Dwango_ForceWebAudio.js
+// ----------------------------------------------------------------------------
+// Copyright (c) 2016 Dwango　Co., Ltd.
+// This software is released under the MIT License.
+// http://opensource.org/licenses/mit-license.php
+// ----------------------------------------------------------------------------
+// Version
+// 1.0.0 2016.11.29
+//=============================================================================
+
+(function(){
+    AudioManager.shouldUseHtml5Audio = function() {
+        return false;
+    };
+})();
